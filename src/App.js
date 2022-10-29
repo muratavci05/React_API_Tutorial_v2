7155898b032df6caba887970964435edd20c161b
muratavci05 <js.muratavci@gmail.com>
2022-10-30 02:15:48 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       api alma
+      </header>
+    </div>
+  );
+}
+
+export default App;
