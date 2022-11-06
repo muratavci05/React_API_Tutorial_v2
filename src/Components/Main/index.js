e6b76cb7b66db20f73e542fd46f365bb8e4289a4
muratavci05 <js.muratavci@gmail.com>
2022-11-06 15:05:29 +0300

    main components modified

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -78,27 +78,59 @@ const Main = (props) => {
 
         for (let i = 0; i < totalPageCount; i++){
           pageComponents.push(
-            <button className="btn btn-outline-primary btn-sm text-center" style={{width:"20px",border:"none"}}>
-              {i+1}
+            <button 
+            key={i}
+            className="btn btn-outline-primary btn-sm text-center" 
+            style={{width:"20px",border:"none"}}
+            >
+                 {i+1}
             </button>
+          )
+        }
+
+        const lengthSelectComponents = []
+        for (let i = 0; i<3; i++){
+          lengthSelectComponents.push(
+            <button 
+            key={i}
+            onClick={()=>setPageLength((i+1)*3)}
+            className="btn btn-outline-danger btn-sm text-center" 
+            style={{width:"20px",border:"none"}}
+            >
+                 { (i+1) * 3 }
+          </button>
           )
         }
 
 
     return (
         <main className="container py-3">
-              <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
-                  Total Page Count: {totalPageCount}
+              <div className="row mb-3 text-center">
+                <div style={{marginTop:"-40px", color:"#B2B2B2"}}>
+                  <h5 style={{color:"gray"}}>
+                  Page Count: {totalPageCount}
+                  </h5>
+               
               &nbsp;
-                  Sayfalar:
+                Page:
                   {pageComponents}
+              &nbsp;
+
+                Rows:
+                  {lengthSelectComponents}
 
                 </div>
 
                     <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
                     {categoryArray}
-                    </div>
-                    <h2 className="display-6 text-center mb-4" style={{color:"red"}}>Bu Projede Backend API'ye Bağlanma Amaçlanmıştır</h2>
+                </div>
+               
+              </div>
+                <h2 
+                className="display-6 text-center mb-4" 
+                style={{color:"red"}}
+                >
+                  Bu Projede Backend API'ye Bağlanma Amaçlanmıştır</h2>
         </main>
     )
 };
