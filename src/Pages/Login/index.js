e53ab7ff5be28988e14cf6b716aa11ffbbd14532
muratavci05 <js.muratavci@gmail.com>
2022-11-07 12:29:53 +0300

    login modified, get post and token

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,21 +1,80 @@
-import React from "react";
+import React,{useState} from "react";
+import useApi from "../../Hooks/useApi"
 
 
-const Login = () => {
+const Login = (props) => {
+
+    const [email, setEmail]=useState("");
+    const [password, setPassword]=useState("");
+
+    const api = useApi();
+
+    const onLoginBtnClick = () => {
+       // alert(`${email} ve ${password}`)
+
+        const postData = {
+            email,password,
+        }
+            console.log("POSTA DATA >>>",postData);
+
+        api.post("/auth/login", postData)
+            .then((res=>{
+                console.log("LOGIN API POST",res);
+                console.log ("LOGIN TOKEN >>> ",res.data.data.token);
+                
+
+                if (res.data.status === "success"){
+                    localStorage.setItem("localStroge TOKEN",res.data.data.token);
+                } else {
+                    alert("Hatalı Şifre veya Kullanıcı Adı");
+                }
+                
+            }))
+            .catch((err)=>{
+                console.log("LOGIN API ERROR", err);
+
+                //her error'da errorMessage döner...!
+                alert(err.response.data.errorMessage)
+            })
+
+    };
 
     return(
 
         <div className="container py-3">
                 <div className="col">
 
-                <label for="email" className="form-label" style={{color:"purple"}}>Email 
+                <label htmlfor="email" className="form-label" style={{color:"purple"}}>Email 
                 </label>
-                <input style={{width:"225px",marginBottom:"15px"}} type="email" className="form-control" id="email" placeholder="you@example.com"/>
-                <label style={{marginBottom:"8px", color:"purple"}} for="password" className="form-label">Password 
+                <input style={{width:"225px",marginBottom:"15px"}} 
+                type="email" 
+                className="form-control" 
+                id="email" 
+                placeholder="you@example.com"
+                value={email}
+                onChange={(event)=>setEmail(event.target.value)}
+                />
+
+                <label style={{marginBottom:"8px", color:"purple"}} htmlfor="password" className="form-label">Password 
                 </label>
-                <input style={{width:"225px",marginBottom:"5px",marginBottom:"5px"}} type="password" className="form-control" id="password" placeholder="password"/>
-                <div>
-                <button type="button" className="btn btn-primary btn-lg btn-block">Gönder</button>
+                <input style={{width:"225px",marginBottom:"5px"}} 
+                type="password" 
+                className="form-control" 
+                id="password" 
+                placeholder="password"
+                value={password}
+                onChange={(event)=>setPassword(event.target.value)}
+                />
+
+                <div className="d-grid gap-2" style={{width:"225px"}}>
+                <button 
+                type="button" 
+                className="btn btn-outline-primary" 
+                style={{}}
+                onClick={onLoginBtnClick}
+                >
+                    Login
+                        </button>
 
                 </div>
             </div>
