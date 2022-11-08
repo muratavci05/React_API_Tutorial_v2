59acf47dca99ca51c0aa57789f936bb631bbb450
muratavci05 <js.muratavci@gmail.com>
2022-11-08 13:02:54 +0300

    CASL install and ability file add

--- src/Ability/ability.js
+++ src/Ability/ability.js
@@ -1,3 +1,14 @@
 import { defineAbility } from '@casl/ability';
 
+const ability = (user)=> defineAbility((can)=>{
+
+
+    //"user_management" (yekti düzzenlemesi yapılacak permission seçtik) >>> userAPI daki kullanıcı permision key'i
+    can("read", "user_management")
+    can("read", "permission_management")
+
+})
+
+export default ability;
+
 
